ddbbc711cb813ac84eaa7af660b79cc41765e726
LilSasi <klarahunor@gmail.com>
2024-12-08 00:13:24 +0200

    New

--- discord_bot.py
+++ discord_bot.py
@@ -32,37 +32,34 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.attachments:  
+    if message.attachments:
         for attachment in message.attachments:
-            
+
             await attachment.save("temp.jpg")
             image = preprocess(Image.open("temp.jpg")).unsqueeze(0).to(device)
 
-            
+
             with torch.no_grad():
-                
+
                 image_features = model.encode_image(image).float()
                 image_features = F.normalize(image_features, p=2, dim=-1)
 
-                
+
                 text_features = model.encode_text(text_tokens).float()
                 text_features = F.normalize(text_features, p=2, dim=-1)
 
-                
+
                 similarity = image_features @ text_features.T
                 print(f"Nyers hasonlóságok: {similarity.cpu().numpy()}")
 
-                
+
                 probs = similarity.softmax(dim=-1).cpu().numpy()
 
-            
+
             print(f"Softmax utáni hasonlóságok: {probs}")
             category = categories[probs.argmax()]
             response = get_response(category)
             await message.channel.send(response)
 
 
-bot.run("Discord Token")
-
-
-#MTMxNDk3Nzk4NzE3ODkyMjAwNA.GmT9bF.mPhaALz1fCrqhxBWZSwGcbSUs8ky2z_fGdAZtI
+bot.run("MTMxNDk3Nzk4NzE3ODkyMjAwNA.Gt518x.jMuJK-e8A7jbQxzVD645ThMCe13wLQ4mxCE7cY")
--- train_model.py
+++ train_model.py
@@ -30,3 +30,4 @@ def train_clip_model():
 
 if __name__ == "__main__":
     train_clip_model()
+    print()
